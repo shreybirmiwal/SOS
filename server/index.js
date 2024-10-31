6b8ce5fbbc08a7050f0c55bbf6956f7816a30a85
Shrey Birmiwal <shreybirmiwal@gmail.com>
2024-10-31 12:39:23 -0500

    Omi endpoint detects if user says 'SOS'

--- server/index.js
+++ server/index.js
@@ -1,12 +1,52 @@
 const express = require("express");
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+app.use(express.json());
 
-app.get("/api", (req, res) => {
+
+// Sample Data
+// POST / your - endpoint ? session_id = abc123 & uid=user123
+
+// [
+//     {
+//         "text": "Segment text",
+//         "speaker": "SPEAKER_00",
+//         "speakerId": 0,
+//         "is_user": false,
+//         "start": 10.0,
+//         "end": 20.0
+//     }
+//     // More recent segments...
+// ]
+
+app.post("/api", async (req, res) => {
+    const { session_id, uid } = req.query;
+    const segments = req.body;
+    var test = "session id" + session_id + "uid" + uid;
+
+    console.log(segments);
+    var full_convo = "";
+
+    for (var i = 0; i < segments.length; i++) {
+        full_convo += segments[i].text + " ";
+    }
+    full_convo = full_convo.toLowerCase();
+
+    if (full_convo.includes("sos")) {
+        //in danger mode
+        console.log("SOS DETECTED");
+    }
+    else {
+        console.log("USER is SAFE");
+    }
+
+    res.json({ message: test });
+});
+
+app.get("/hello", (req, res) => {
     res.json({ message: "Hello from Express!" });
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
--- sos_frontend/src/App.js
+++ sos_frontend/src/App.js
@@ -4,18 +4,33 @@ function App() {
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
-    fetch("/api")
+    const sampleData = [
+      {
+        text: "Segment text SOS",
+        speaker: "SPEAKER_00",
+        speakerId: 0,
+        is_user: false,
+        start: 10.0,
+        end: 20.0,
+      },
+    ];
+
+    fetch("/api?session_id=abc123&uid=user123", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(sampleData),
+    })
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then((data) => setData(data.message))
+      .catch((err) => console.error("Error:", err));
   }, []);
 
   return (
-
     <div className="bg-blue-400">
       <p>{!data ? "Loading..." : data}</p>
-
     </div>
-
   );
 }
 
